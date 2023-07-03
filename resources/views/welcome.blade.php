be253d77d504f5a077aebd80fe5ecde7799fecc2
Amir <amir.abdhamid@araken.biz>
2023-07-03 12:56:06 +0800

    try last

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel testing testing testing6 testing283</title>
+        <title>Laravel testing testing testing6 testing2snasnd3</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
